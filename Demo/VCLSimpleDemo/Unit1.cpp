4263c31a4022539c9d5ab482024d4f1377602527
G. C <giulicard@gmail.com>
2020-05-29 01:26:43 +0200

    Added demo project VCLSimpleDemo

---
+++ Demo/VCLSimpleDemo/Unit1.cpp
@@ -0,0 +1,111 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include <anafestica/FileVersionInfo.h>
+
+#include "Unit1.h"
+
+using Anafestica::TFileVersionInfo;
+
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm1( Owner, StoreOpts::All, nullptr )
+{
+}
+//---------------------------------------------------------------------------
+
+__fastcall TForm1::TForm1( TComponent* Owner, StoreOpts StoreOptions,
+							   Anafestica::TConfigNode* const RootNode )
+	: TConfigRegistryForm( Owner, StoreOptions, RootNode )
+{
+	selectedFontName_ = Label1->Font->Name;
+	SetupCaption();
+	LoadFontListUIControl();
+	RestoreProperties();
+	SelectCurrentFont();
+}
+//---------------------------------------------------------------------------
+
+__fastcall TForm1::~TForm1()
+{
+	try {
+		SaveProperties();
+	}
+	catch ( ... ) {
+	}
+}
+//---------------------------------------------------------------------------
+
+String TForm1::GetModuleFileName()
+{
+	return GetModuleName( reinterpret_cast<unsigned>( HInstance ) );
+}
+//---------------------------------------------------------------------------
+
+void TForm1::SetupCaption()
+{
+	TFileVersionInfo const Info( GetModuleFileName() );
+	Caption =
+		Format(
+			_T( "%s, Ver %s" ),
+			ARRAYOFCONST( (
+				Application->Title,
+				Info.ProductVersion
+			) )
+		);
+}
+//---------------------------------------------------------------------------
+
+void TForm1::LoadFontListUIControl()
+{
+	comboboxFontName->Items->Assign( Screen->Fonts );
+}
+//---------------------------------------------------------------------------
+
+void TForm1::SelectCurrentFont()
+{
+	comboboxFontName->ItemIndex =
+		comboboxFontName->Items->IndexOf( SelectedFontName );
+}
+//---------------------------------------------------------------------------
+
+void TForm1::SetSelectedFontName( String Val )
+{
+	if ( selectedFontName_ != Val ) {
+		selectedFontName_ = Val;
+		lblClock->Font->Name = Val;
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::comboboxFontNameChange(TObject *Sender)
+{
+	SelectedFontName = comboboxFontName->Text;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Timer1Timer(TObject *Sender)
+{
+	lblClock->Caption = Now().FormatString( _T( "hh.nn.ss ") );
+}
+//---------------------------------------------------------------------------
+
+void TForm1::RestoreProperties()
+{
+	RESTORE_LOCAL_PROPERTY( SelectedFontName );
+}
+//---------------------------------------------------------------------------
+
+void TForm1::SaveProperties() const
+{
+	SAVE_LOCAL_PROPERTY( SelectedFontName );
+}
+//---------------------------------------------------------------------------
+
---
+++ Demo/VCLSimpleDemo/Unit1.h
@@ -0,0 +1,56 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+
+//---------------------------------------------------------------------------
+
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.StdCtrls.hpp>
+
+#include <anafestica/PersistFormVCL.h>
+#include <anafestica/CfgRegistrySingleton.h>
+
+//---------------------------------------------------------------------------
+
+using TConfigRegistryForm =
+    Anafestica::TPersistFormVCL<Anafestica::TConfigRegistrySingleton>;
+
+//---------------------------------------------------------------------------
+
+class TForm1 : public TConfigRegistryForm
+{
+__published:	// IDE-managed Components
+	TComboBox *comboboxFontName;
+	TLabel *Label1;
+	TLabel *lblClock;
+	TTimer *Timer1;
+	void __fastcall Timer1Timer(TObject *Sender);
+	void __fastcall comboboxFontNameChange(TObject *Sender);
+private:	// User declarations
+	String selectedFontName_;
+
+	static String GetModuleFileName();
+	void SetupCaption();
+	void RestoreProperties();
+	void SaveProperties() const;
+	void LoadFontListUIControl();
+	void SelectCurrentFont();
+	void SetSelectedFontName( String Val );
+
+	__property String SelectedFontName = {
+		read = selectedFontName_,
+		write = SetSelectedFontName
+	};
+public:		// User declarations
+	__fastcall TForm1( TComponent* Owner );
+	__fastcall TForm1( TComponent* Owner, StoreOpts StoreOptions,
+					   Anafestica::TConfigNode* const RootNode = nullptr );
+	__fastcall ~TForm1();
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif

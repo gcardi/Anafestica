20cd569abf4f5809d4ca66a2602aa63b62b55fd9
G. C <giulicard@gmail.com>
2020-05-29 01:31:46 +0200

    Fixed tabs len in VCLSimpleDemo

--- Demo/VCLSimpleDemo/Unit1.cpp
+++ Demo/VCLSimpleDemo/Unit1.cpp
@@ -16,96 +16,96 @@ TForm1 *Form1;
 //---------------------------------------------------------------------------
 
 __fastcall TForm1::TForm1(TComponent* Owner)
-	: TForm1( Owner, StoreOpts::All, nullptr )
+    : TForm1( Owner, StoreOpts::All, nullptr )
 {
 }
 //---------------------------------------------------------------------------
 
 __fastcall TForm1::TForm1( TComponent* Owner, StoreOpts StoreOptions,
-							   Anafestica::TConfigNode* const RootNode )
-	: TConfigRegistryForm( Owner, StoreOptions, RootNode )
+                               Anafestica::TConfigNode* const RootNode )
+    : TConfigRegistryForm( Owner, StoreOptions, RootNode )
 {
-	selectedFontName_ = Label1->Font->Name;
-	SetupCaption();
-	LoadFontListUIControl();
-	RestoreProperties();
-	SelectCurrentFont();
+    selectedFontName_ = Label1->Font->Name;
+    SetupCaption();
+    LoadFontListUIControl();
+    RestoreProperties();
+    SelectCurrentFont();
 }
 //---------------------------------------------------------------------------
 
 __fastcall TForm1::~TForm1()
 {
-	try {
-		SaveProperties();
-	}
-	catch ( ... ) {
-	}
+    try {
+        SaveProperties();
+    }
+    catch ( ... ) {
+    }
 }
 //---------------------------------------------------------------------------
 
 String TForm1::GetModuleFileName()
 {
-	return GetModuleName( reinterpret_cast<unsigned>( HInstance ) );
+    return GetModuleName( reinterpret_cast<unsigned>( HInstance ) );
 }
 //---------------------------------------------------------------------------
 
 void TForm1::SetupCaption()
 {
-	TFileVersionInfo const Info( GetModuleFileName() );
-	Caption =
-		Format(
-			_T( "%s, Ver %s" ),
-			ARRAYOFCONST( (
-				Application->Title,
-				Info.ProductVersion
-			) )
-		);
+    TFileVersionInfo const Info( GetModuleFileName() );
+    Caption =
+        Format(
+            _T( "%s, Ver %s" ),
+            ARRAYOFCONST( (
+                Application->Title,
+                Info.ProductVersion
+            ) )
+        );
 }
 //---------------------------------------------------------------------------
 
 void TForm1::LoadFontListUIControl()
 {
-	comboboxFontName->Items->Assign( Screen->Fonts );
+    comboboxFontName->Items->Assign( Screen->Fonts );
 }
 //---------------------------------------------------------------------------
 
 void TForm1::SelectCurrentFont()
 {
-	comboboxFontName->ItemIndex =
-		comboboxFontName->Items->IndexOf( SelectedFontName );
+    comboboxFontName->ItemIndex =
+        comboboxFontName->Items->IndexOf( SelectedFontName );
 }
 //---------------------------------------------------------------------------
 
 void TForm1::SetSelectedFontName( String Val )
 {
-	if ( selectedFontName_ != Val ) {
-		selectedFontName_ = Val;
-		lblClock->Font->Name = Val;
-	}
+    if ( selectedFontName_ != Val ) {
+        selectedFontName_ = Val;
+        lblClock->Font->Name = Val;
+    }
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TForm1::comboboxFontNameChange(TObject *Sender)
 {
-	SelectedFontName = comboboxFontName->Text;
+    SelectedFontName = comboboxFontName->Text;
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TForm1::Timer1Timer(TObject *Sender)
 {
-	lblClock->Caption = Now().FormatString( _T( "hh.nn.ss ") );
+    lblClock->Caption = Now().FormatString( _T( "hh.nn.ss ") );
 }
 //---------------------------------------------------------------------------
 
 void TForm1::RestoreProperties()
 {
-	RESTORE_LOCAL_PROPERTY( SelectedFontName );
+    RESTORE_LOCAL_PROPERTY( SelectedFontName );
 }
 //---------------------------------------------------------------------------
 
 void TForm1::SaveProperties() const
 {
-	SAVE_LOCAL_PROPERTY( SelectedFontName );
+    SAVE_LOCAL_PROPERTY( SelectedFontName );
 }
 //---------------------------------------------------------------------------
 
--- Demo/VCLSimpleDemo/Unit1.h
+++ Demo/VCLSimpleDemo/Unit1.h
@@ -22,33 +22,33 @@ using TConfigRegistryForm =
 
 class TForm1 : public TConfigRegistryForm
 {
-__published:	// IDE-managed Components
-	TComboBox *comboboxFontName;
-	TLabel *Label1;
-	TLabel *lblClock;
-	TTimer *Timer1;
-	void __fastcall Timer1Timer(TObject *Sender);
-	void __fastcall comboboxFontNameChange(TObject *Sender);
-private:	// User declarations
-	String selectedFontName_;
+__published:    // IDE-managed Components
+    TComboBox *comboboxFontName;
+    TLabel *Label1;
+    TLabel *lblClock;
+    TTimer *Timer1;
+    void __fastcall Timer1Timer(TObject *Sender);
+    void __fastcall comboboxFontNameChange(TObject *Sender);
+private:    // User declarations
+    String selectedFontName_;
 
-	static String GetModuleFileName();
-	void SetupCaption();
-	void RestoreProperties();
-	void SaveProperties() const;
-	void LoadFontListUIControl();
-	void SelectCurrentFont();
-	void SetSelectedFontName( String Val );
+    static String GetModuleFileName();
+    void SetupCaption();
+    void RestoreProperties();
+    void SaveProperties() const;
+    void LoadFontListUIControl();
+    void SelectCurrentFont();
+    void SetSelectedFontName( String Val );
 
-	__property String SelectedFontName = {
-		read = selectedFontName_,
-		write = SetSelectedFontName
-	};
-public:		// User declarations
-	__fastcall TForm1( TComponent* Owner );
-	__fastcall TForm1( TComponent* Owner, StoreOpts StoreOptions,
-					   Anafestica::TConfigNode* const RootNode = nullptr );
-	__fastcall ~TForm1();
+    __property String SelectedFontName = {
+        read = selectedFontName_,
+        write = SetSelectedFontName
+    };
+public:     // User declarations
+    __fastcall TForm1( TComponent* Owner );
+    __fastcall TForm1( TComponent* Owner, StoreOpts StoreOptions,
+                       Anafestica::TConfigNode* const RootNode = nullptr );
+    __fastcall ~TForm1();
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TForm1 *Form1;

02eab9de7c870aac8931a172ebd1ba59939a7fb4
Giuliano Cardinali <giulicard@gmail.com>
2020-10-30 10:50:06 +0100

    Update Demo to test Explicit Types setting in JSON serializer

--- Demo/VCLSimpleDemo/Unit1.cpp
+++ Demo/VCLSimpleDemo/Unit1.cpp
@@ -109,6 +109,58 @@ void TForm1::SaveProperties() const
 {
     SAVE_LOCAL_PROPERTY( SelectedFontName );
     //GetConfigNode()[_D( "Sub" )].PutItem( _D( "Item" ), 100 );
+    GetConfigNode()[_D( "Sub" )][_D( "Sotto" )].PutItem( _D( "Item" ), 100 );
+
+    using StringCont = std::vector<String>;
+    using BytesCont = std::vector<Byte>;
+
+    unsigned int             u   = 10;
+    long                     l   = 11L;
+    unsigned long            ul  = 12UL;
+    char                     c   = '@';
+    unsigned char            uc  = 0x34;
+    short                    s   = -32000;
+    unsigned short           us  = 65000;
+    long long                ll  = -45LL;
+    unsigned long long       ull = 0x1000000000000;
+    bool                     b   = true;
+    System::TDateTime        dt  = Now();
+    float                    flt = 61.34F;
+    double                   dbl = M_PI;
+    System::Currency         cur = 1200.45;
+    StringCont               sv  { { _D( "Uno" ), _D( "Due" ), _D( "Tre" ), } };
+    System::Sysutils::TBytes dab;
+    dab.Length = 3;
+    dab[0] = 0x45;
+    dab[1] = 0x56;
+    dab[2] = 0x67;
+    BytesCont                vb  = { 0xFE, 0xCA, 0x39, 0x43 };
+
+
+
+
+    SAVE_LOCAL_PROPERTY( u );
+    SAVE_LOCAL_PROPERTY( l );
+    SAVE_LOCAL_PROPERTY( ul );
+    SAVE_LOCAL_PROPERTY( c );
+    SAVE_LOCAL_PROPERTY( uc );
+    SAVE_LOCAL_PROPERTY( s );
+    SAVE_LOCAL_PROPERTY( us );
+    SAVE_LOCAL_PROPERTY( ll );
+    SAVE_LOCAL_PROPERTY( ull );
+    SAVE_LOCAL_PROPERTY( b );
+    SAVE_LOCAL_PROPERTY( dt );
+    SAVE_LOCAL_PROPERTY( flt );
+    SAVE_LOCAL_PROPERTY( dbl );
+    SAVE_LOCAL_PROPERTY( cur );
+    SAVE_LOCAL_PROPERTY( sv );
+    SAVE_LOCAL_PROPERTY( dab );
+    SAVE_LOCAL_PROPERTY( vb );
+
+
+
+
+
 }
 //---------------------------------------------------------------------------
 
